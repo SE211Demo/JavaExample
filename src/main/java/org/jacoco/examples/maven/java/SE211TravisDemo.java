af9040bef930febcfbb1730db6d47b8cf28b228a
AmrSabbagh <amrG.sabbagh@gmail.com>
2017-04-19 15:10:22 +0300

    Update SE211TravisDemo.java

--- src/main/java/org/jacoco/examples/maven/java/SE211TravisDemo.java
+++ src/main/java/org/jacoco/examples/maven/java/SE211TravisDemo.java
@@ -3,7 +3,7 @@ package se211travisdemo;
 public class SE211TravisDemo {
 
     public static void main(String[] args) {
-        System.out.println("this is the travis demo in univ45");
+        System.out.println("this is in the se211 class");
     }
 
 }
