65f681dffe8536354417010317c092e8fca78cfe
AmrSabbagh <amrG.sabbagh@gmail.com>
2017-04-19 14:54:04 +0300

    ex

---
+++ src/main/java/org/jacoco/examples/maven/java/ex.java
@@ -0,0 +1,12 @@
+package exampleinclass;
+
+public class ex {
+
+    public String sIsEx(boolean x) {
+        if (x) {
+            return "yay";
+        } else {
+            return "nay";
+        }
+    }
+}
